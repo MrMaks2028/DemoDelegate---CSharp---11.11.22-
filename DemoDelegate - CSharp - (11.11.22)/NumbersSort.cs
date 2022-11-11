0a8822902693ddcb78c881310efc357257b751ef
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-11-11 20:06:05 +0500

    Добавьте файлы проекта.

---
+++ DemoDelegate - CSharp - (11.11.22)/NumbersSort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDelegate___CSharp____11._11._22_
+{
+    internal class NumbersSort
+    {
+        
+    }
+}
---
+++ DemoDelegate - CSharp - (11.11.22)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDelegate___CSharp____11._11._22_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
